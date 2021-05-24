84ce642a0384c41d210879292b50f711bacedbdd
Shakuri Shahin <shahin_shakuri@ens.univ-artois.fr>
2021-05-24 18:15:02 +0200

    La documentation mise à jour

--- Labyrinthe/doc/package-search-index.js
+++ Labyrinthe/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"composants"},{"l":"test"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"composants"},{"l":"joueurs"},{"l":"partie"},{"l":"test"}];updateSearchResults();
